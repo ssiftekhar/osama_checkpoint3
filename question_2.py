bc41c39dc95b1eda11d4fb2061ab08c0e6418e36
Osama Iftekhar <68809388+ssiftekhar@users.noreply.github.com>
2021-05-14 14:10:25 +0200

    Update question_2.py

--- question_2.py
+++ question_2.py
@@ -1,13 +1,7 @@
-
-def to_camel_case(text): 
-    text1 = '' 
-    text1 += text[0].upper() 
-    for i in range(1, len(text)): 
-        if (text[i] == ' '): 
-            text1 += text[i + 1].upper() 
-            i += 1
-        elif(text[i - 1] != ' '): 
-            text1 += text[i]  
-    print(text1)
-
-print(to_camel_case('denne testen gikk ikke bra'))
+# turn a string to camel case text
+def to_camel_case(text):
+    camel_text = ''
+    text_list = text.split(' ')
+    for word in text_list:
+        camel_text = camel_text + word [0].upper()+word[1:].lower()
+    return(camel_text)
