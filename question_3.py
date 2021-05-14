0b9d824e9a9b1bcf52c9467b7e4f982fba778776
Osama Iftekhar <68809388+ssiftekhar@users.noreply.github.com>
2021-05-14 14:54:48 +0200

    Update question_3.py

--- question_3.py
+++ question_3.py
@@ -1,18 +1,31 @@
+# Question 3:
+# The company SuperSale3000 want to rank its sales team at the end of every week. 
+# Each call gives the seller 10 points, each meeting 30 points and each sale 100 points. 
+# There is also a 100-point bonus for each, if you make more than 150 calls, or 20 meetings or 5 sales.
+# Create a new file called “question_3.py”.
 
-Jordan_belfort = {'calls': 178, 'meetings':17,  'sales':6}
+# They already have the data for each employee saved in a dictionary.
+# At the top of the file set the following example dictionary for Jodan Belfort:
+# Jordan_belfort = {‘calls’: 178, ‘meetings’:17,  ‘sales’:6}
+# Add the following logic to calculate the total score from the dictionary and add it to the same dictionary using the key ‘score’
 
-def add_point():
-    calls = 0
-    meetings = 0
-    sales = 0
-    total_score = []
-    for key in Jordan_belfort.items():
-        total_score.append(calls+meetings+sales)
-        if calls:
-            calls = calls + 10
-        elif meetings:
-            meetings = meetings + 30
-        elif sales:
-            sales = sales + 100
-         
 
+jordan_belfort = {'calls': 178, 'meetings':17, 'sales':6}
+
+CALLS_M = 10
+MEETINGS_M = 30
+SALES_M = 100
+
+score = 0
+
+score = score + jordan_belfort['calls']*CALLS_M
+score = score + jordan_belfort['meetings']*MEETINGS_M
+score = score + jordan_belfort['sales']*SALES_M
+if jordan_belfort['calls']>150:
+    score = score + 100
+if jordan_belfort['meetings']>20:
+    score = score + 100
+if jordan_belfort['sales']>5:
+    score = score + 100
+
+jordan_belfort['score'] = score
